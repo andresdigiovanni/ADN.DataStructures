940e9c2f838be886f2f0cff2419a0ed1c5255729
Andrés <andresdigiovanni@gmail.com>
2018-09-19 19:06:25 +0200

    Added documentation

--- src/ADN.DataStructures/src/BidirHashtable.cs
+++ src/ADN.DataStructures/src/BidirHashtable.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace ADN.DataStructures
 {
+    /// <summary>
+    /// Represents a collection of key/value pairs that are organized based on both hash code.
+    /// </summary>
     public class BidirHashtable
     {
         private Hashtable _htFwd = new Hashtable();
@@ -13,6 +16,11 @@ namespace ADN.DataStructures
         static readonly object _locker = new object();
 
 
+        /// <summary>
+        /// Adds an element with the specified key and value into the BidirHashtable.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="val">The value of the element to add.</param>
         public void Add(object key, object val)
         {
             lock (_locker)
@@ -22,6 +30,11 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key whose value to get.</param>
+        /// <returns>The value associated with the specified key.</returns>
         public dynamic Get(object key)
         {
             lock (_locker)
@@ -30,6 +43,11 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Gets the key associated with the specified value.
+        /// </summary>
+        /// <param name="val">The value whose key to get.</param>
+        /// <returns>The key associated with the specified value.</returns>
         public dynamic GetKey(object val)
         {
             lock (_locker)
@@ -38,6 +56,10 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Gets the number of key/value pairs contained in the BidirHashtable.
+        /// </summary>
+        /// <returns>The number of key/value pairs contained in the BidirHashtable.</returns>
         public int Count()
         {
             lock (_locker)
@@ -46,6 +68,11 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Sets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key whose value to set.</param>
+        /// <param name="val">The value of the element to set.</param>
         public void Set(object key, object val)
         {
             lock (_locker)
@@ -55,6 +82,11 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Determines whether the BidirHashtable contains a specific key.
+        /// </summary>
+        /// <param name="key">The key to locate in the BidirHashtable.</param>
+        /// <returns>True if the BidirHashtable contains an element with the specified key; otherwise, false.</returns>
         public bool Contains(object key)
         {
             lock (_locker)
@@ -63,6 +95,11 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Determines whether the BidirHashtable contains a specific value.
+        /// </summary>
+        /// <param name="val">The value to locate in the BidirHashtable.</param>
+        /// <returns>True if the BidirHashtable contains an element with the specified value; otherwise, false.</returns>
         public bool ContainsValue(object val)
         {
             lock (_locker)
@@ -71,6 +108,9 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Removes all elements from the BidirHashtable.
+        /// </summary>
         public void Clear()
         {
             lock (_locker)
@@ -80,6 +120,10 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Removes the element with the specified key from the BidirHashtable.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
         public void Remove(object key)
         {
             lock (_locker)
@@ -90,6 +134,10 @@ namespace ADN.DataStructures
             }
         }
 
+        /// <summary>
+        /// Removes the element with the specified value from the BidirHashtable.
+        /// </summary>
+        /// <param name="val">The value of the element to remove.</param>
         public void RemoveValue(object val)
         {
             lock (_locker)
